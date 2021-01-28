2efa19d03eb55112f682bef12261835018101a8e
wrogoz <wrogoz@gmail.com>
2021-01-28 07:59:55 +0100

    connection issue solved

--- movies-service/middleware/auth.js
+++ movies-service/middleware/auth.js
@@ -3,19 +3,22 @@ const axios = require("axios")
 
 const auth =  async(req, res, next) => {
 try {
-  axios.post('0.0.0.0:3000/auth', {
+const u = await axios.post('http://app:3000/auth', {
     username: "basic-thomas",
     password: "sR-_pcoow-27-6PAwCD8"
   })
   .then(function (response) {
-    console.log(`:)`);
+    return ':)'
   })
   .catch(function (error) {
     console.log(error);
   });
+  console.log(u)
+
 } catch (error) {
 console.log(error)
 }
+
 next()
 
 
