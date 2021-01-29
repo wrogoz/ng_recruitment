4145fba7a51619138857885c5d7bbc0b0cfbadf2
wrogoz <wrogoz@gmail.com>
2021-01-29 12:55:32 +0100

    app js code ref

--- movies-service/app.js
+++ movies-service/app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 require('dotenv').config()
-const db=require('./db_config/db_config');
+require('./db_config/db_config');
 const app = express();
 const moviesRoute = require('./routes/moviesRoute');
 app.use(express.json());
 
-db.connect((err)=>{
-	if(err){
-		console.error("error connecting: " + err.stack);
-		return process.exit(22);
-	}
-	console.log("connected as id " + db.threadId);
-});
 
 app.use('/movies',moviesRoute);
 
--- movies-service/db_config/db_config.js
+++ movies-service/db_config/db_config.js
@@ -8,5 +8,12 @@ var db = mysql.createConnection({
     multipleStatements: true,
 
 });
+db.connect((err)=>{
+	if(err){
+		console.error("error connecting: " + err.stack);
+		return process.exit(22);
+	}
+	console.log("connected as id " + db.threadId);
+});
 
 module.exports=db
